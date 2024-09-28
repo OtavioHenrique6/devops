c0900ee9f01414df5d800debbdb0bc4968a65e69
OtavioHenrique6 <otaviohenrikethoughts@gmail.com>
2024-09-27 23:17:22 -0300

    sintaxe

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,7 @@ const Home = () => {
 
   return (
     <C.Container>
-      <C.Title>Seja Bem-Vindo !!</C.Title>
+      <C.Title>Seja muito Bem-Vindo !!</C.Title>
       <Button Text="Logout" onClick={() => [signout(), navigate("/")]}>
         Sair
       </Button>
